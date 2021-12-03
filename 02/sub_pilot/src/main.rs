6cb5f2b4a9dc425eadfd9bdc6c01eb5a521030de
jan0ski <mccormickt9@gmail.com>
2021-12-03 17:30:37 -0500

    Add day2 part 2

--- 02/sub_pilot/src/main.rs
+++ 02/sub_pilot/src/main.rs
@@ -24,11 +24,34 @@ struct Instruction {
     distance: i32,
 }
 
-/// The location of our submarine
 #[derive(Debug)]
 struct Location {
     pub x: i32,
     pub y: i32,
+    pub aim: i32,
+}
+
+impl Location {
+    pub fn adjust_location_p1(&mut self, inst: &Instruction) {
+        match inst.direction {
+            // Our depth decreases if we go up, increases if we go down
+            Direction::Up => self.y -= inst.distance,
+            Direction::Down => self.y += inst.distance,
+            Direction::Forward => self.x += inst.distance,
+        }
+    }
+    pub fn adjust_location_p2(&mut self, inst: &Instruction) {
+        match inst.direction {
+            // Our aim decreases if we go up, increases if we go down
+            Direction::Up => self.aim -= inst.distance,
+            Direction::Down => self.aim += inst.distance,
+            // our depth and distance are affected by moving forward, based on our aim
+            Direction::Forward => {
+                self.x += inst.distance;
+                self.y += self.aim * inst.distance;
+            }
+        }
+    }
 }
 
 /// Parse an instruction from a line in the file
@@ -51,7 +74,7 @@ fn parse_instruction(line: String) -> Option<Instruction> {
 }
 
 fn part_one(directions_file: &str) {
-    let mut pos = Location { x: 0, y: 0 };
+    let mut pos = Location { x: 0, y: 0, aim: 0 };
 
     // Read our instructions
     read_file(directions_file)
@@ -62,16 +85,28 @@ fn part_one(directions_file: &str) {
         // translate the instruction string into something we understand
         .map(|x| parse_instruction(x.to_string()).unwrap())
         // change our position based on the instruction
-        .for_each(|i| match i.direction {
-            // Our depth decreases if we go up, increases if we go down
-            Direction::Up => pos.y -= i.distance,
-            Direction::Down => pos.y += i.distance,
-            Direction::Forward => pos.x += i.distance,
-        });
+        .for_each(|i| pos.adjust_location_p1(&i));
 
     println!("Part One: {}", pos.x * pos.y);
 }
 
+fn part_two(directions_file: &str) {
+    let mut loc = Location { x: 0, y: 0, aim: 0 };
+
+    read_file(directions_file)
+        .unwrap()
+        .as_str()
+        // line by line
+        .lines()
+        // translate the instruction string into something we understand
+        .map(|x| parse_instruction(x.to_string()).unwrap())
+        // change our position based on the instruction
+        .for_each(|x| loc.adjust_location_p2(&x));
+
+    println!("Part Two: {}", loc.x * loc.y);
+}
+
 fn main() {
-    part_one("resources/directions.txt");
+    part_one("resources/instructions.txt");
+    part_two("resources/instructions.txt");
 }
